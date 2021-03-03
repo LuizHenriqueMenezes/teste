0fd22e00e8476fc0ba4f1894be1e5c94af5f052a
Luiz Henrique <lmenezesmessias@gmail.com>
2021-03-03 10:01:07 -0300

    Finalização

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Este progama é um teste do Aluno Luiz Henrique Menezes da Etec Adolpho Berezin.
+*/
+using System;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Luiz Henrique");//minha primeira vez
+            Console.WriteLine();
+            Console.WriteLine(2020);
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("VAI CORINTHIANS");
+            Console.WriteLine("Fim");
+
+            return;
+        }
+    }
+}
